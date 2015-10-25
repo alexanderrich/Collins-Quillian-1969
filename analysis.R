d5f9ddc75123031bc28c03c60a224366a18e7b66
Alex Rich <alexandersrich@gmail.com>
2015-10-25 13:47:19 -0400

    remove two clear outliers
    still inverted V shapes for both sentence types, but a much clearer
    difference between level 0 and level 1 for property sentences

--- analysis.R
+++ analysis.R
@@ -2,7 +2,17 @@ library("ggplot2")
 library("dplyr")
 
 trialdata <- read.csv("participants.csv")
+
+indiv_perf <- trialdata %>% group_by(uniqueid) %>%
+  summarize(rt=mean(rt), performance=mean(hit))
+
+indiv_perf %>%
+  ggplot(aes(rt, performance)) + geom_point()
+
+outliers <- indiv_perf %>% filter(performance < .85) %>% .$uniqueid
+
 trialdata %>% filter(hit) %>%
+  filter(!(uniqueid %in% outliers)) %>%
   filter(truth) %>%
   group_by(level, type) %>%
   summarize(rt=mean(rt)) %>%
